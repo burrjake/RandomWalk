c45316be5b049629b8cb6d4f36cc0d65792dc074
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-10 08:27:01 -0600

    Add project files.

---
+++ RandomWalk.vb
@@ -0,0 +1,22 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Text
+
+Module RandomWalk
+
+    Sub Main()
+        Dim value As Integer
+        Dim userInput As String
+
+        Do While userInput <> "q"
+            For i = 1 To 10
+                Randomize()
+                value = CInt(Int((Rnd() * 6) + 1))
+                Console.WriteLine(value)
+            Next
+            userInput = Console.ReadLine()
+            Console.Clear()
+        Loop
+    End Sub
+
+End Module
