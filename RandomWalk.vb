00776b991ff704598a7407676e7d5f3f9a0c4fe9
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-10 08:55:24 -0600

    add GetRandomNumber Function

--- RandomWalk.vb
+++ RandomWalk.vb
@@ -5,18 +5,39 @@ Option Compare Text
 Module RandomWalk
 
     Sub Main()
-        Dim value As Integer
-        Dim userInput As String
+        ' Dim randomNumber As Integer
 
-        Do While userInput <> "q"
-            For i = 1 To 10
-                Randomize()
-                value = CInt(Int((Rnd() * 6) + 1))
-                Console.WriteLine(value)
-            Next
-            userInput = Console.ReadLine()
-            Console.Clear()
-        Loop
+        ' randomNumber = GetRandomNumber(1, 10)
+        'Console.WriteLine(randomNumber)
+        'Console.ReadLine()
+
     End Sub
 
+    Sub RandomExample()
+        'Dim value As Integer
+        'Dim userInput As String
+
+        'Do While userInput <> "q"
+        'For i = 1 To 10
+        'Randomize()
+        'value = CInt(Int((Rnd() * 6) + 1))
+        'Console.WriteLine(value)
+        'Next
+        'userInput = Console.ReadLine()
+        'Console.Clear()
+        'Loop
+    End Sub
+
+    Function GetRandomNumber(ByVal minimum As Integer, maximum As Integer) As Integer
+        Dim value As Integer
+
+
+
+
+        Randomize()
+        'value = CInt(Int((Rnd() * 6) + 1))
+        value = CInt(maximum * Rnd())
+        Console.WriteLine(value)
+        Return value
+    End Function
 End Module
